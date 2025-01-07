e52e82986909e4c23587cb3ee233b8be755d9036
Arjun Kapoor <arjunkapoor@xiphos-2.local>
2025-01-08 02:23:02 +0530

    Setup trpc

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -10,6 +10,7 @@ import Link from "next/link"
 import { useForm } from "react-hook-form"
 import {zodResolver} from "@hookform/resolvers/zod"
 import { AuthCredentialsValidator, TAuthCredentialsValidator } from "@/lib/validators/account-credentials-validators"
+import { trpc } from "@/trpc/client"
 
 const Page = () => {
 
@@ -20,7 +21,11 @@ const Page = () => {
         resolver: zodResolver(AuthCredentialsValidator)
     })
 
+    const {data} = trpc.anyApiRoute.useQuery()
+    console.log(data)
+
     const onSubmit = ({email, password}:  TAuthCredentialsValidator) => {
+        console.log(email,password)
 
     }
 
